50a1b3b61d6f270162d480e4a43c5d128542ffa0
srijnasri <srijnasri1999@gmail.com>
2023-12-28 16:30:59 +0530

    Initial Commit

---
+++ TechNews/TechNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechNewsApp.swift
+//  TechNews
+//
+//  Created by Srijnasri Negi on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TechNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
